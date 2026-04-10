c4941ff116301370a9305fbc11251b508dbdb309
luisitsilva <luisitsilva@protonmail.com>
2026-04-10 18:27:49 -0400

    destroying test

--- main.tf
+++ main.tf
@@ -26,36 +26,36 @@ provider "aws" {
 }
 
 # Add .gitignore file in this directory with the terraform.tfvars
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-02f986bab3de34d0d"
-  instance_type = "t3.micro"
+# resource "aws_instance" "tc_instance" {
+#   ami           = "ami-02f986bab3de34d0d"
+#   instance_type = "t3.micro"
 
-  tags = {
-    Name = "TC-triggered-instance"
-    State = "Updates"
-  }
-}
+#   tags = {
+#     Name = "TC-triggered-instance"
+#     State = "Updates"
+#   }
+# }
 
-resource "aws_iam_user" "pedro" {
-  name = "Pedro"
-}
+# resource "aws_iam_user" "pedro" {
+#   name = "Pedro"
+# }
 
-resource "aws_iam_user" "joao" {
-  name = "Joao"
-}
+# resource "aws_iam_user" "joao" {
+#   name = "Joao"
+# }
 
-resource "aws_iam_user" "Antonio" {
-  name = "Antonio"
-}
+# resource "aws_iam_user" "Antonio" {
+#   name = "Antonio"
+# }
 
-resource "aws_iam_user" "loja1" {
-  name = "Loja1"
-}
+# resource "aws_iam_user" "loja1" {
+#   name = "Loja1"
+# }
 
-resource "aws_iam_user" "casas0" {
-  name = "casas0"
-}
+# resource "aws_iam_user" "casas0" {
+#   name = "casas0"
+# }
 
-resource "aws_iam_user" "casas1" {
-  name = "casas1"
-}
+# resource "aws_iam_user" "casas1" {
+#   name = "casas1"
+# }
