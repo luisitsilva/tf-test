182720fe98dfa9f10524b871df038b7165309376
luisitsilva <luisitsilva@protonmail.com>
2026-04-10 16:47:10 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+## PUT TERRAFORM CLOUD BLOCK HERE!  ##
+
+terraform {
+  cloud {
+    organization = "flashintell_luissilva"
+
+    workspaces {
+      name = "tfcloudiacflash"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.01"
+    }
+  }
+}
+
+# Variable blocks directly within the main.tf. No arguments necessary.
+variable "region" {}
+
+# provider arguments call on the variables which then call on terraform.tfvars for the values.
+provider "aws" {
+  region     = var.region 
+}
+
+# Add .gitignore file in this directory with the terraform.tfvars
+resource "aws_instance" "tc_instance" {
+  ami           = "ami-02f986bab3de34d0d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "TC-triggered-instance"
+  }
+}
